9fc55701a4bf61110f3e1be27c794a61bee9e665
Christina Lampart <Christina.Lampart@student.uibk.ac.at>
2021-03-25 16:38:57 +0100

    stop objekt erzeugt und verwendet

--- nz/main.js
+++ nz/main.js
@@ -1,14 +1,25 @@
+let stop = {
+    nr: 23,
+    name: "Taupo",
+    lat: -38.684444,
+    lng: 176.070833,
+    user: "Christinal2012",
+    wikipedia: "https://en.wikipedia.org/wiki/Taup%C5%8D",
+};
+
+console.log(stop);
+
 const map = L.map("map", {
-    center: [-38.684444, 176.070833],
+    center: [stop.lat, stop.lng],
     zoom: 13,
     layers: [
         L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
     ]
 });
 
-let mrk = L.marker([-38.684444, 176.070833]).addTo(map);
+let mrk = L.marker([stop.lat, stop.lng]).addTo(map);
 mrk.bindPopup("Taupo").openPopup();
 
 
 //WMTS-Services
-console.log(document.querySelector("#map"));
+//console.log(document.querySelector("#map"));
