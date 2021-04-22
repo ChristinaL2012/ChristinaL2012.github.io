fd7624a496e31aee9793ef8ec73495c52ac7752f
Christina Lampart <Christina.Lampart@student.uibk.ac.at>
2021-04-22 18:18:32 +0200

    attribut für die farbe

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -53,7 +53,7 @@ L.control.scale({
 let getColor = (value,colorRamp) => {
     console.log("Wert:", value, "Palette:", colorRamp);
     for (let rule of colorRamp){
-        if(value >= rule.min && val < rule.max) {
+        if(value >= rule.min && value < rule.max) {
             return rule.col;
         }
     }
@@ -62,8 +62,9 @@ let getColor = (value,colorRamp) => {
 
 let newLabel = (coords, options) => {
     let color = getColor(options.value, options.colors)
+    console.log("Wert", options.value, "bekommt Farbe", color);
     let label = L.divIcon({
-        html: `<div>${options.value}</div>`,
+        html: `<div style = "background-color: ${color}>${options.value}</div>`,
         className: "text-label"
     })
     let marker = L.marker([coords[1],coords[0]], {
